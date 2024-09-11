1a112247048ed1129f0b127851c994bd24bed0ac
Mert Eray <meerteray@gmail.com>
2024-09-11 20:24:01 +0300

    Initial Commit

---
+++ SpotifyMini-iOS/SpotifyMini_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyMini_iOSApp.swift
+//  SpotifyMini-iOS
+//
+//  Created by Mert Eray on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyMini_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
